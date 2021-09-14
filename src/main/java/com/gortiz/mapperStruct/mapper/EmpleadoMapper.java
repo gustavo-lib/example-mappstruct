53215f4f4a4c0496e8d8abb38b40542be1147f19
gortiz <ggortiz09@gmail.com>
2021-09-14 00:37:27 -0300

    fix

--- src/main/java/com/gortiz/mapperStruct/mapper/EmpleadoMapper.java
+++ src/main/java/com/gortiz/mapperStruct/mapper/EmpleadoMapper.java
@@ -10,13 +10,19 @@ import com.gortiz.mapperStruct.model.Empleado;
 @Mapper
 public interface EmpleadoMapper {
 	//con atributos de distintos nombre
-	
+	//dto-->entity
 	@Mappings({
-		@Mapping(target = "nombre", source = "empleadoDTO.nombreEmpleado"),
+		@Mapping(target = "nombreEmpleado", source = "empleadoDTO.nombreEmpleadoDTO"),
 		@Mapping(target = "fechaIngreso", source = "empleadoDTO.fechaIngreso", dateFormat = "dd-MM-yyyy HH:mm:ss")
 	})
 	Empleado empleadoDTOToEmpleado(EmpleadoDTO empleadoDTO);
 	
+	
+	//entity-->dto
+	@Mappings({// de la class EmpleadoDTO por el atributo nombreEmpleadoDTO tomael valor de la fuente class Empleado.nombreEmpleado 
+		@Mapping(target = "nombreEmpleadoDTO", source = "empleado.nombreEmpleado"),
+		@Mapping(target = "fechaIngreso", source = "empleado.fechaIngreso", dateFormat = "dd-MM-yyyy HH:mm:ss")
+	})
 	EmpleadoDTO empleadoToEmpleadoDTO(Empleado empleado);
 
 }

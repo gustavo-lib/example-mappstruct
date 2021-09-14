27f76c19ceb6876f0b45be204f3930d994aa2a5a
gortiz <ggortiz09@gmail.com>
2021-09-14 00:22:10 -0300

    toma para la propiedad nombre tome la propiedad empleadoDTO.nombreEmpleado

--- src/main/java/com/gortiz/mapperStruct/mapper/EmpleadoMapper.java
+++ src/main/java/com/gortiz/mapperStruct/mapper/EmpleadoMapper.java
@@ -1,6 +1,8 @@
 package com.gortiz.mapperStruct.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 import com.gortiz.mapperStruct.dto.EmpleadoDTO;
 import com.gortiz.mapperStruct.model.Empleado;
@@ -9,7 +11,10 @@ import com.gortiz.mapperStruct.model.Empleado;
 public interface EmpleadoMapper {
 	//con atributos de distintos nombre
 	
-	
+	@Mappings({
+		@Mapping(target = "nombre", source = "empleadoDTO.nombreEmpleado"),
+		@Mapping(target = "fechaIngreso", source = "empleadoDTO.fechaIngreso", dateFormat = "dd-MM-yyyy HH:mm:ss")
+	})
 	Empleado empleadoDTOToEmpleado(EmpleadoDTO empleadoDTO);
 	
 	EmpleadoDTO empleadoToEmpleadoDTO(Empleado empleado);

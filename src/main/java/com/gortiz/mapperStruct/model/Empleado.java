e760538bd5fac262db180e330595673615eceaee
gortiz <ggortiz09@gmail.com>
2021-09-14 00:02:39 -0300

    update class

--- src/main/java/com/gortiz/mapperStruct/model/Empleado.java
+++ src/main/java/com/gortiz/mapperStruct/model/Empleado.java
@@ -1,5 +1,50 @@
 package com.gortiz.mapperStruct.model;
 
+import com.gortiz.mapperStruct.dto.LugarDTO;
+
 public class Empleado {
+
+	private Integer id;
+	private String nombreEmpleado;
+	private LugarDTO sede;
+	private String fechaIngreso;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombreEmpleado() {
+		return nombreEmpleado;
+	}
+
+	public void setNombreEmpleado(String nombreEmpleado) {
+		this.nombreEmpleado = nombreEmpleado;
+	}
+
+	public LugarDTO getSede() {
+		return sede;
+	}
+
+	public void setSede(LugarDTO sede) {
+		this.sede = sede;
+	}
+
+	public String getFechaIngreso() {
+		return fechaIngreso;
+	}
+
+	public void setFechaIngreso(String fechaIngreso) {
+		this.fechaIngreso = fechaIngreso;
+	}
+
+	@Override
+	public String toString() {
+		return "EmpleadoDTO [id=" + id + ", nombreEmpleado=" + nombreEmpleado + ", sede=" + sede + ", fechaIngreso="
+				+ fechaIngreso + "]";
+	}
 
 }
--- src/main/java/com/gortiz/mapperStruct/model/Lugar.java
+++ src/main/java/com/gortiz/mapperStruct/model/Lugar.java
@@ -1,5 +1,30 @@
 package com.gortiz.mapperStruct.model;
 
 public class Lugar {
+	
+	private Integer id;
+	private String nombre;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "SedeDTO [id=" + id + ", nombre=" + nombre + "]";
+	}
+
 
 }

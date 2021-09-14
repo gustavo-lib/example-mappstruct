5f4d581610f8f29bc719dfc1cb945ad95ef33e51
gortiz <ggortiz09@gmail.com>
2021-09-13 23:16:31 -0300

    adding class dto

---
+++ src/main/java/com/gortiz/mapperStruct/dto/EmpleadoDTO.java
@@ -0,0 +1,48 @@
+package com.gortiz.mapperStruct.dto;
+
+public class EmpleadoDTO {
+	private Integer id;
+	private String nombreEmpleado;
+	private LugarDTO sede;
+	private String fechaIngreso;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombreEmpleado() {
+		return nombreEmpleado;
+	}
+
+	public void setNombreEmpleado(String nombreEmpleado) {
+		this.nombreEmpleado = nombreEmpleado;
+	}
+
+	public LugarDTO getSede() {
+		return sede;
+	}
+
+	public void setSede(LugarDTO sede) {
+		this.sede = sede;
+	}
+
+	public String getFechaIngreso() {
+		return fechaIngreso;
+	}
+
+	public void setFechaIngreso(String fechaIngreso) {
+		this.fechaIngreso = fechaIngreso;
+	}
+
+	@Override
+	public String toString() {
+		return "EmpleadoDTO [id=" + id + ", nombreEmpleado=" + nombreEmpleado + ", sede=" + sede + ", fechaIngreso="
+				+ fechaIngreso + "]";
+	}
+
+
+}

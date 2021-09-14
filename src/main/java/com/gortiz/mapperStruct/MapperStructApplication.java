b0234999e0c762bb56116fff49b05a7383c34506
gortiz <ggortiz09@gmail.com>
2021-09-13 23:09:14 -0300

    first commit

---
+++ src/main/java/com/gortiz/mapperStruct/MapperStructApplication.java
@@ -0,0 +1,13 @@
+package com.gortiz.mapperStruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapperStructApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapperStructApplication.class, args);
+	}
+
+}
